366261e2524723130c30f6b0458ca9a844e24dfa
Hinamizawa <themoonbunnys@gmail.com>
2024-02-04 12:23:58 -0400

    Push

---
+++ static/js/hina_temp_bug_fix.js
@@ -0,0 +1,29 @@
+const hina_mode = 1; //1 = untestable version, 2 = testable version
+if (hina_mode == 1){
+    document.addEventListener('DOMContentLoaded', function() {
+        const badgeBlocks = document.querySelectorAll('.select-left.badge-block');
+        const seenBadges = new Map();
+
+        badgeBlocks.forEach(block => {
+            const badgeName = block.querySelector('.badge-name').textContent.trim().toLowerCase();
+            if (seenBadges.has(badgeName)) {
+                block.parentNode.removeChild(block);
+            } else {
+                seenBadges.set(badgeName, true);
+            }
+        });
+    });
+}
+else if(hina_mode == 2) {
+const badgeBlocks = document.querySelectorAll('.select-left.badge-block');
+const seenBadges = new Map();
+badgeBlocks.forEach(block => {
+    const badgeName = block.querySelector('.badge-name').textContent.trim().toLowerCase();
+    if (seenBadges.has(badgeName)) {
+        block.parentNode.removeChild(block);
+    } else {
+        seenBadges.set(badgeName, true);
+    }
+});
+
+}
